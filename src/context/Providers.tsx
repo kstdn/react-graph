2f5c3eb2f5c3465a46e413ec262503e20383c9b3
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-07-08 00:07:09 +0300

    Clean up Providers

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,19 +1,17 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useState } from 'react';
 import {
   ExpandedGraphNode,
   isExpanded,
-  togglePath
-} from "../expanded-graph.util";
-import { GraphNodeDef } from "../models/GraphNodeDef";
-import { getExpandedContext } from "./ExpandedContext";
-import { getNodesContext } from "./NodesContext";
-// import { getSelectedNodeContext } from "./SelectedNodeContext";
+  togglePath,
+} from '../expanded-graph.util';
+import { GraphNodeDef } from '../models/GraphNodeDef';
+import { getExpandedContext } from './ExpandedContext';
+import { getNodesContext } from './NodesContext';
 
-const expandedStateKey = "expanded-state";
+const expandedStateKey = 'expanded-state';
 
 type Props<TId> = {
   nodes: Map<TId, GraphNodeDef<TId>>;
-  // onNodeSelected: (nodeId: TId | undefined) => any;
   persistExpandedState: boolean;
   children: ReactNode;
   loadNodesAsyncFunc?: (ids: TId[]) => Promise<GraphNodeDef<TId>[]>;
@@ -27,13 +25,9 @@ export function Providers<TId>({
 }: Props<TId>) {
   const [expandedGraph, setExpandedGraph] = useState<ExpandedGraphNode<TId>[]>(
     persistExpandedState
-      ? JSON.parse(localStorage.getItem(expandedStateKey) ?? "[]")
+      ? JSON.parse(localStorage.getItem(expandedStateKey) ?? '[]')
       : []
   );
-
-  // const [selectedNodeId, setSelectedNodeId] = useState<TId | undefined>(
-  //   undefined
-  // );
 
   const onExpandToggled = (path: TId[]) => {
     const updatedExpandedGraph = togglePath(path, expandedGraph);
@@ -45,14 +39,6 @@ export function Providers<TId>({
     setExpandedGraph(updatedExpandedGraph);
   };
 
-  // const handleNodeSelectionChange = (nodeId: TId | undefined) => {
-  //   setSelectedNodeId(() => {
-  //     onNodeSelected(nodeId);
-  //     return nodeId;
-  //   })
-  // }
-
-  // const SelectedNodeContext = getSelectedNodeContext<TId>();
   const NodesContext = getNodesContext<TId>();
   const ExpandedContext = getExpandedContext<TId>();
 
@@ -69,37 +55,29 @@ export function Providers<TId>({
 
     return loadNodesAsyncFunc
       ? loadNodesAsyncFunc(toLoad).then(result => {
-          result.forEach(node => nodes.set(node.id, node))
+          result.forEach(node => nodes.set(node.id, node));
         })
       : Promise.resolve([]);
   };
 
   return (
-    // <SelectedNodeContext.Provider
-    //   value={{
-    //     selectedNodeId: selectedNodeId,
-    //     setSelectedNodeId: handleNodeSelectionChange,
-    //     isSelected: (id: TId) => id === selectedNodeId,
-    //   }}
-    // >
-      <NodesContext.Provider
+    <NodesContext.Provider
+      value={{
+        nodes,
+        getNode,
+        loadNodesAsync,
+      }}
+    >
+      <ExpandedContext.Provider
         value={{
-          nodes,
-          getNode,
-          loadNodesAsync,
+          expandedGraph,
+          isExpanded: (path: TId[]) => isExpanded(path, expandedGraph),
+          onExpandToggled,
         }}
       >
-        <ExpandedContext.Provider
-          value={{
-            expandedGraph,
-            isExpanded: (path: TId[]) => isExpanded(path, expandedGraph),
-            onExpandToggled,
-          }}
-        >
-          {children}
-        </ExpandedContext.Provider>
-      </NodesContext.Provider>
-    // </SelectedNodeContext.Provider>
+        {children}
+      </ExpandedContext.Provider>
+    </NodesContext.Provider>
   );
 }
 
